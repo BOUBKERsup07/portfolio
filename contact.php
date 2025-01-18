5f7da08013e0fa8a9532bb224ae4c18ae90ab615
BOUBKERsup07 <nikaimkjf289@gmail.com>
2025-01-19 00:12:36 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,6 +7,11 @@
 </head>
 <body>
     <?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     
